01ca51861f8fbd034996995a9083ba5fe99a5883
Edward Blundell <efblundell@hotmail.co.uk>
2013-11-11 21:15:02 +0000

    commit234

--- index.php
+++ index.php
@@ -1,44 +1,88 @@
 <?php
-/**
- * Requests collector.
- *
- *  This file collects requests if:
- *	- no mod_rewrite is available or .htaccess files are not supported
- *  - requires App.baseUrl to be uncommented in app/Config/core.php
- *	- app/webroot is not set as a document root.
- *
- * PHP 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c), Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
 
 /**
- *  Get Cake's root directory
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Mage
+ * @package    Mage
+ * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-define('APP_DIR', 'app');
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(__FILE__));
-define('WEBROOT_DIR', 'webroot');
-//define('WEBROOT_DIR', 'webroot'.'/magpiebox_example');
-define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
 
-/**
- * This only needs to be changed if the "cake" directory is located
- * outside of the distributed structure.
- * Full path to the directory containing "cake". Do not add trailing directory separator
- */
-if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
+if (version_compare(phpversion(), '5.2.0', '<')===true) {
+    echo  '<div style="font:12px/1.35em arial, helvetica, sans-serif;">
+<div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
+<h3 style="margin:0; font-size:1.7em; font-weight:normal; text-transform:none; text-align:left; color:#2f2f2f;">
+Whoops, it looks like you have an invalid PHP version.</h3></div><p>Magento supports PHP 5.2.0 or newer.
+<a href="http://www.magentocommerce.com/install" target="">Find out</a> how to install</a>
+ Magento using PHP-CGI as a work-around.</p></div>';
+    exit;
 }
 
-require APP_DIR . DS . WEBROOT_DIR . DS . 'index.php';
+/**
+ * Error reporting
+ */
+error_reporting(E_ALL | E_STRICT);
+
+/**
+ * Compilation includes configuration file
+ */
+define('MAGENTO_ROOT', getcwd());
+
+$compilerConfig = MAGENTO_ROOT . '/includes/config.php';
+if (file_exists($compilerConfig)) {
+    include $compilerConfig;
+}
+
+$mageFilename = MAGENTO_ROOT . '/app/Mage.php';
+$maintenanceFile = 'maintenance.flag';
+
+if (!file_exists($mageFilename)) {
+    if (is_dir('downloader')) {
+        header("Location: downloader");
+    } else {
+        echo $mageFilename." was not found";
+    }
+    exit;
+}
+
+if (file_exists($maintenanceFile)) {
+    include_once dirname(__FILE__) . '/errors/503.php';
+    exit;
+}
+
+require_once $mageFilename;
+
+Varien_Profiler::enable();
+
+#if (isset($_SERVER['MAGE_IS_DEVELOPER_MODE'])) {
+    Mage::setIsDeveloperMode(true);
+#}
+
+ini_set('display_errors', 1);
+
+umask(0);
+
+/* Store or website code */
+$mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : '';
+
+/* Run store or run website */
+$mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
+
+Mage::run($mageRunCode, $mageRunType);
